733db7c5654b5919b48e8a3e3ba551b2ac857e5a
1-jeongeun <alysia.je.lee@gmail.com>
2022-10-28 14:54:57 -0400

    Update Board.java

--- 2015_tp1/src/tp1/Board.java
+++ 2015_tp1/src/tp1/Board.java
@@ -1,8 +1,8 @@
-//IFT2015 TP1
-//Auteures: Jeongeun Lee (20215747)
+// IFT2015 TP1
+// Auteurs: Jeongeun Lee (20215747)
 //          Grace Lee (20174147)
+// Date:    Le 28 octobre 2022
 
-//
 package tp1;
 
 import java.util.ArrayList;
@@ -25,6 +25,7 @@ public class Board {
         this.inactiveBomb = new ArrayList<Integer>();
     }
 
+    // create the random number between 0, 1 and 2 in the board
     protected static int getNumber() 
     {
         int min = 0;
@@ -37,6 +38,7 @@ public class Board {
         return value;
     }
 
+    // create the board of the n x m size that we assign (rowNum x colNum)
     protected ArrayList<ArrayList<Integer>> makeBoard() 
     {
         ArrayList<ArrayList<Integer>> board = new ArrayList<ArrayList<Integer>>();
@@ -53,7 +55,7 @@ public class Board {
         return board;
     }
 
-
+    // print out the board with a space between each number 
     public void printBoard() 
     {
         for (int i = 0; i < this.rowNum; i++) {
@@ -68,9 +70,9 @@ public class Board {
         System.out.println("");
     }
 
+    // 
     public int activateBomb() 
     {
-
         this.inactiveBomb.clear();
 
         for (int i = 0; i < this.rowNum; i++) {
@@ -93,47 +95,48 @@ public class Board {
 
         return isThereBomb();
     }
-    
+   
     protected int isThereBomb()
     {
         for (int i = 0; i < this.rowNum; i++) {
             for (int j = 0; j < this.colNum; j++) {
                 if (this.board.get(i).get(j) == 1) {
                     if (inactiveBomb.isEmpty()) {
-                        return -1;
+                        return -1;      // return -1 if the inactiveBomb array is empty but if there are still inactive bombs in the board
                     } else {
-                        return 1;
+                        return 1;       // return 1 if there are inactive bombs in the array
                     }
                 }
             }
         }
-        return 0;
+        return 0;       // return 0 if there is no more inactive bomb in the array
     }
 
+    // check around the active bombs and if there is an inactive bomb, add its position to the inactiveBomb arraylist
     protected void checkAround(int posRow, int posCol) 
     {
 
         if (posRow > 0) {
             if (this.board.get(posRow - 1).get(posCol) == 1) {
-                this.inactiveBomb.add((posRow - 1) * this.colNum + posCol);
+                this.inactiveBomb.add((posRow - 1) * this.colNum + posCol);     // check left
             }
         }
 
         if (posRow < this.rowNum - 1) {
             if (this.board.get(posRow + 1).get(posCol) == 1) {
-                this.inactiveBomb.add((posRow + 1) * this.colNum + posCol);
+                this.inactiveBomb.add((posRow + 1) * this.colNum + posCol);     // check right
             }
         }
 
         if (posCol > 0) {
             if (this.board.get(posRow).get(posCol - 1) == 1) {
-                this.inactiveBomb.add(posRow * this.colNum + (posCol - 1));
+                this.inactiveBomb.add(posRow * this.colNum + (posCol - 1));     // check bottom
             }
         }
 
         if (posCol < this.colNum - 1) {
             if (this.board.get(posRow).get(posCol + 1) == 1) {
-                this.inactiveBomb.add(posRow * this.colNum + (posCol + 1));
+                this.inactiveBomb.add(posRow * this.colNum + (posCol + 1));     // check top
             }
         }
     }
